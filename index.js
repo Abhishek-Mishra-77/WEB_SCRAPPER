4a10662eb989934bbc97a2fa552c041625de484c
unknown <abhishekmishra992016@gmail.com>
2024-10-21 17:28:50 +0530

    header less option in the launch

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false
+    });
     const page = await browser.newPage();
-    await page.goto('https://linktr.ee/abhishekmishra07');
-    // await page.goto('https://www.wikipedia.org/');
+    // await page.goto('https://linktr.ee/abhishekmishra07');
+    await page.goto('https://github.com/Abhishek-Mishra-77');
     await page.setViewport({ width: 1080, height: 1024 });
     await page.screenshot({ path: 'test.png' });
     await browser.close();
